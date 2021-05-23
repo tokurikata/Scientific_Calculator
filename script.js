17f0321d58b8b0ed8ce3bf6865e4c779c838841f
Nehal Tembhurne <tembhurnenehal@gmail.com>
2021-05-23 15:45:44 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+type="text"
+function sin() {
+    document.calculator.result.value=Math.sin(document.calculator.result.value*Math.PI/180)
+}
+function cos() {
+    document.calculator.result.value=Math.cos(document.calculator.result.value*Math.PI/180)
+}
+function tan() {
+    document.calculator.result.value=Math.tan(document.calculator.result.value*Math.PI/180)
+}
+function backspc() {
+    var a = document.calculator.result.value;
+    document.calculator.result.value = a.substr(0, a.length-1);
+}
+function square() {
+    document.calculator.result.value=Math.pow(document.calculator.result.value, 2)
+}
+function cube() {
+    document.calculator.result.value=Math.pow(document.calculator.result.value, 3)
+}
+function sqrt2() {
+    document.calculator.result.value=Math.pow(document.calculator.result.value, 1/2)
+}
+function sqrt3() {
+    document.calculator.result.value=Math.pow(document.calculator.result.value, 1/3)
+}
+function number(value) {
+    document.calculator.result.value += value;
+}
+function remv() {
+    document.calculator.result.value = " ";
+}
+function equal() {
+    document.calculator.result.value = eval(document.calculator.result.value);
+}
